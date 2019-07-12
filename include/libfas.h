c705264e0174c07c457fe83fa0a02f9d3c784c8a
Andrea Fioraldi <andreafioraldi@gmail.com>
2019-07-12 11:48:41 +0200

    fas_init() and fas_open() implementation in libfas

--- include/libfas.h
+++ include/libfas.h
@@ -4,6 +4,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+/* Initialize the FAS device. Put this in yout main() before starting any
+   thread. Returns the file descriptor of the FAS device. */
+int fas_init();
+
+/* Open a session on a file. Unsupported flags are:
+    - O_TMPFILE
+    - O_PATH */
 int fas_open(char* pathname, int flags, mode_t mode);
 
 #endif
--- libfas/libfas.c
+++ libfas/libfas.c
@@ -3,7 +3,21 @@
 
 #include <sys/ioctl.h>
 
+static int fas_dev_fd;
+
+int fas_init() {
+
+  fas_dev_fd = open(FAS_FILE_NAME, 0);
+  return fas_dev_fd;
+}
+
 int fas_open(char* pathname, int flags, mode_t mode) {
 
-  return 0;
+  struct fas_open_args args = {
+    pathname,
+    flags,
+    mode
+  };
+  
+  return ioctl(fas_dev_fd, FAS_IOCTL_OPEN, &args);
 }
