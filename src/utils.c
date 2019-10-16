3c3ef0fd1fc688d3533b4092bfe9a2bae4688cc9
Andrea Fioraldi <andreafioraldi@gmail.com>
2019-10-16 11:07:12 +0200

    fas_is_subpath routine

--- src/utils.c
+++ src/utils.c
@@ -34,3 +34,28 @@ int fas_filp_copy(struct file* src, struct file* dst) {
 
 }
 
+int fas_is_subpath(char* super_pathname, char* sub_pathname) {
+
+  struct path path1, path2;
+  int r = 0;
+  
+  int follow_param = 0;
+  if (follow_links) follow_param = LOOKUP_FOLLOW;
+  
+  if (!super_pathname || kern_path(super_pathname, 0, &path1))
+    goto end_is_subpath;
+  
+  if (!sub_pathname || kern_path(sub_pathname, follow_param, &path2))
+    goto path1_cleanup;
+
+  r = d_ancestor(path1->dentry, path2->dentry) != NULL;
+
+  path_put(&path2);
+
+path1_cleanup:
+  path_put(&path1);
+
+end_is_subpath:
+  return r;
+
+}
